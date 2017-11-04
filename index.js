f98f9eb33207e4be4607bdb77ef85270f1172105
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-04 20:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,24 +12,7 @@ var a = 5, b=4, c=3, d=2, e=1, f="the end";
 a="new word"
 
 
-function sayhellojohn() {
-  console.log("Hello John!")
-}
-
-function sayhelloerick () {
-  console.log ("Hello Erick!")
-}
-
-function sayhellotom (){
-  console.log("Hello Tom!")
-}
-
-function dosomething(thing){
-  console.log(thing)
-
-}
-
-function add(x, y){
+function add(x, y) {
   return x + y
 }
 
