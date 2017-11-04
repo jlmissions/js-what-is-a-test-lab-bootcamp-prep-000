594eaf67b4af13f1e0e0fd1281b39890d656b054
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-04 20:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ var height = 39
 var message = ` is ${height} inches tall ${name}`
 
 module.exports = {name, height, message}
+
+var a = 5, b=4, c=3, d=2, e=1, f="the end";
+
+//later on
+
+a="new word"
+
+
+function sayhellojohn() {
+  console.log("Hello John!")
+}
+
+function sayhelloerick () {
+  console.log ("Hello Erick!")
+}
+
+function sayhellotom (){
+  console.log("Hello Tom!")
+}
+
+function dosomething(thing){
+  console.log(thing)
+
+}
+
+function add(x, y){
+  return x + y
+}
+
+add(1, 2)
